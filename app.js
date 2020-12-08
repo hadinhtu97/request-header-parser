1c3d19d2f86bffd9cdeb5e0cbea104910a286c1b
hadinhtu97 <hadinhtu97@gmail.com>
2020-12-08 16:18:44 +0700

    update app.js

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ app.use(cors({ optionsSuccessStatus: 200 }));
 
 app.get('/', (req, res) => res.sendFile(__dirname + "/views/index.html"));
 
+app.set('trust proxy', true)
+app.get('/api/whoami', (req, res) => {
+    res.json({
+        ipadress: req.ip,
+        language: req.headers["accept-language"],
+        software: req.headers["user-agent"]
+    })
+})
+
 app.listen(port, () => {
     console.log('Your app is listening on port ' + port);
 })
